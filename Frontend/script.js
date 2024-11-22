c3297263b37d129773de020b59f8e6b22ffd2da8
AnaPaulaLechuga <anapaula.lechuga12@gmail.com>
2024-11-22 13:36:08 -0700

    Agregar ID de las imágenes de personajes y un poco de JavaScript

--- Frontend/script.js
+++ Frontend/script.js
@@ -1,78 +1,80 @@
-var canvas = document.getElementById('miCanvas');
-            var ctx = canvas.getContext('2d');
-            var color = "black";
-            // Crea una nueva imagen
-            var imagen = new Image();
+var indiceEspecie = 0;
+var indiceVariedad = 0;
+var indiceCasco = 0;
+var indiceCamisa = 0;
+var indicePantalon = 0;
 
-            // Establece la fuente de la imagen
-            imagen.src = 'Humano-01';
+const especies = ['Elfo', 'Humano', 'Enano'];
+const imagenes = {
+    Elfo: {
+        Cuerpo: ['Personajes/Elfo-01.png', 'Personajes/Elfo-02.png', 'Personajes/Elfo-03.png', 'Personajes/Elfo-04.png'],
+        Casco: ['Personajes/Casco-Elfo-01.png', 'Personajes/Casco-Elfo-02.png'],
+        Camisa: ['Personajes/Camisa-Elfo-01.png', 'Personajes/Camisa-Elfo-02.png'],
+        Pantalon: ['Personajes/Pantalon-Elfo-01.png', 'Personajes/Pantalon-Elfo-02.png']
+    },
+    Humano: {
+        Cuerpo: ['Personajes/Humano-01.png', 'Personajes/Humano-02.png', 'Personajes/Humano-03.png', 'Personajes/Humano-04.png'],
+        Casco: ['Personajes/Casco-Humano-01.png', 'Personajes/Casco-Humano-02.png'],
+        Camisa: ['Personajes/Camisa-Humano-01.png', 'Personajes/Camisa-Humano-02.png'],
+        Pantalon: ['Personajes/Pantalon-Humano-01.png', 'Personajes/Pantalon-Humano-02.png']
+    },
+    Enano: {
+        Cuerpo: ['Personajes/Enano-01.png', 'Personajes/Enano-02.png', 'Personajes/Enano-03.png', 'Personajes/Enano-04.png'],
+        Casco: ['Personajes/Casco-Enano-01.png', 'Personajes/Casco-Enano-02.png'],
+        Camisa: ['Personajes/Camisa-Enano-01.png', 'Personajes/Camisa-Enano-02.png'],
+        Pantalon: ['Personajes/Pantalon-Enano-01.png', 'Personajes/Pantalon-Enano-02.png']
+    }
+};
 
-            // Dibuja la imagen en el canvas cuando se haya cargado
-            imagen.onload = function() {
-                ctx.drawImage(imagen, 0, 0, canvas.width, canvas.height);
-            }
-            ;
+function cambiarEspecie() {
+    indiceEspecie = (indiceEspecie + 1) % especies.length;
+    indiceVariedad = 0;
+    indiceCasco = 0;
+    indiceCamisa = 0;
+    indicePantalon = 0;
+    actualizarImagen();
+}
 
-            var dibujando = false;
-            // Variable para rastrear si el usuario está dibujando
+function cambiarVariedad() {
+    indiceVariedad = (indiceVariedad + 1) % imagenes[especies[indiceEspecie]].Cuerpo.length;
+    actualizarImagen();
+}
 
-            // Evento que se activa cuando se presiona el botón del mouse
-            canvas.addEventListener('mousedown', function(e) {
-                dibujando = true;
-                ctx.strokeStyle = color;
-                ctx.beginPath();
-                ctx.moveTo(e.clientX - canvas.getBoundingClientRect().left, e.clientY - canvas.getBoundingClientRect().top);
-            });
+function cambiarImagen(tipo) {
+    if (tipo === 'Casco') {
+        indiceCasco = (indiceCasco + 1) % imagenes[especies[indiceEspecie]][tipo].length;
+    } else if (tipo === 'Camisa') {
+        indiceCamisa = (indiceCamisa + 1) % imagenes[especies[indiceEspecie]][tipo].length;
+    } else if (tipo === 'Pantalon') {
+        indicePantalon = (indicePantalon + 1) % imagenes[especies[indiceEspecie]][tipo].length;
+    }
+    actualizarImagen();
+}
 
-            // Evento que se activa cuando se mueve el mouse
-            canvas.addEventListener('mousemove', function(e) {
-                if (dibujando) {
-                    ctx.lineTo(e.clientX - canvas.getBoundingClientRect().left, e.clientY - canvas.getBoundingClientRect().top);
-                    ctx.stroke();
-                }
-            });
+function actualizarImagen() {
+    const especie = especies[indiceEspecie];
+    document.getElementById('cuerpo').src = imagenes[especies].Cuerpo[indiceVariedad];
+    document.getElementById('camisa').src = imagenes[especies].Camisa[indiceCamisa];
+    document.getElementById('pantalon').src = imagenes[especies].Pantalon[indicePantalon];
+    document.getElementById('casco').src = imagenes[especies].Casco[indiceCasco];
+}
 
-            // Evento que se activa cuando se suelta el botón del mouse
-            canvas.addEventListener('mouseup', function() {
-                dibujando = false;
-                ctx.closePath();
-            });
+function resetearImagen() {
+    indiceVariedad = 0;
+    indiceCasco = 0;
+    indiceCamisa = 0;
+    indicePantalon = 0;
+    actualizarImagen();
+}
 
-            // Evento que se activa cuando se sale del canvas con el botón del mouse presionado
-            canvas.addEventListener('mouseout', function() {
-                dibujando = false;
-                ctx.closePath();
-            });
+function cambiarImagenAleatoria() {
+    const especie = especies[indiceEspecie];
+    indiceVariedad = Math.floor(Math.random() * imagenes[especie].Cuerpo.length);
+    indiceCasco = Math.floor(Math.random() * imagenes[especieActual].Casco.length);
+    indiceCamisa = Math.floor(Math.random() * imagenes[especieActual].Camisa.length);
+    indicePantalon = Math.floor(Math.random() * imagenes[especieActual].Pantalon.length);
+    actualizarImagen();
+}
 
-            function cambiarImagen(nuevaImagen) {
-                imagen.src = nuevaImagen;
-                imagen.onload = function () {
-                    ctx.drawImage(imagen, 0, 0, canvas.width, canvas.height);
-                };
-            }
-
-            let pg1Btn = document.getElementById("image1");
-            let pg2Btn = document.getElementById("image2");
-            let pg3Btn = document.getElementById("image3");
-            let pg4Btn = document.getElementById("image4");
-            let pg5Btn = document.getElementById("image5");
-
-            pg1Btn.onclick = function() {
-                cambiarImagen ("Colorear_Mesa de trabajo 1.jpg");
-            }
-
-            pg2Btn.onclick = function() {
-                cambiarImagen ("Colorear_Mesa de trabajo 1 copia.jpg");
-            }
-
-            pg3Btn.onclick = function() {
-                cambiarImagen ("Colorear_Mesa de trabajo 1 copia 2.jpg");
-            }
-
-            pg4Btn.onclick = function() {
-                cambiarImagen ("Colorear_Mesa de trabajo 1 copia 3.jpg");
-            }
-
-            pg5Btn.onclick = function() {
-                cambiarImagen ("Colorear_Mesa de trabajo 1 copia 4.jpg");
-            }
+// Inicializar la imagen al cargar la página
+window.onload = actualizarImagen;
